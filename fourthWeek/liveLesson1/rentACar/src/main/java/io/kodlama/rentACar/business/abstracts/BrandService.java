3aa15878e96e4cbc170df97f3235ba49e2bf1570
Ali Bozlak <113685537+alibozlak@users.noreply.github.com>
2022-10-26 10:02:19 +0300

    Deleted some folder

--- fourthWeek/liveLesson1/rentACar/src/main/java/io/kodlama/rentACar/business/abstracts/BrandService.java
+++
@@ -1,9 +0,0 @@
-package io.kodlama.rentACar.business.abstracts;
-
-import java.util.List;
-
-import io.kodlama.rentACar.entities.concretes.Brand;
-
-public interface BrandService {
-    List<Brand> getAll();
-}
--- fourthWeek/liveLesson1/rentACar/src/main/java/io/kodlama/rentACar/dataAccess/abstracts/BrandRepository.java
+++
@@ -1,9 +0,0 @@
-package io.kodlama.rentACar.dataAccess.abstracts;
-
-import java.util.List;
-
-import io.kodlama.rentACar.entities.concretes.Brand;
-
-public interface BrandRepository {
-    List<Brand> getAll();
-}
